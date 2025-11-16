392ce343de9df4d09ffbabe924345314c90a6c60
GlacialVitality <glacialvitality@gmail.com>
2025-11-15 19:52:13 -0500

    init

---
+++ include/BeamGoRoundPlanet.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Syati.h"
+
+class BeamGoRoundBeam : public LiveActor {
+public:
+    BeamGoRoundBeam(MtxPtr);
+
+    virtual ~BeamGoRoundBeam();
+    virtual void init(const JMapInfoIter &);
+    virtual void draw() const;
+    virtual void calcAndSetBaseMtx();
+    virtual void updateHitSensor(HitSensor *);
+    virtual void attackSensor(HitSensor *, HitSensor *);
+
+    VolumeDrawer* mModelDrawer;        // 0x8C
+    ModelObj* mBloomModel;                // 0x90
+    MtxPtr mBeamJointMtx;                   // 0x94
+    TVec3f mPosition;
+};
+
+class BeamGoRoundPlanet : public MapObjActor {
+public:
+    BeamGoRoundPlanet(const char *);
+
+    virtual ~BeamGoRoundPlanet();
+    virtual void init(const JMapInfoIter &);
+    virtual void connectToScene(const MapObjActorInitInfo &);
+
+    void initBeam();
+    void exeWait();
+
+    BeamGoRoundBeam** mBeams;           // 0xC4
+    CollisionParts* _C8;
+    CollisionParts* _CC;
+    s32 mBeamCount;
+    bool mBeamsInitialized;
+};
